39bb329db7d183e4c79b673562eef1dcedc8b58a
pedroportacio <56261114+pedroportacio@users.noreply.github.com>
2019-12-01 18:54:47 -0300

    Commit Pedro
    Coisas CSS

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import png3 from "../Img/png3.png";
 import coco from "../Img/coco.png";
+import lol from "../Img/lol.png";
 import "../styles.css";
 
 function Principal() {
@@ -9,7 +10,12 @@ function Principal() {
     <div>
       <h1 class="main"> Veja os principais jogos e streamers da twitch</h1>
       <img src={png3} width="300" />
+      <img src={lol} width="350" />
       <img src={coco} width="150" />
+      <h4 class="main">
+        Clique Nos Botões em azul na barra verde para selecionar o que deseja
+        assistir!
+      </h4>
     </div>
   );
 }
