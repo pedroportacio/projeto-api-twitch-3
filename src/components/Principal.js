7ee7a2db488762b67f5afad03ad36b0296b62584
pedroportacio <56261114+pedroportacio@users.noreply.github.com>
2019-12-01 23:23:03 -0300

    Commit Final
    Paddings e cores

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -4,19 +4,20 @@ import png3 from "../Img/png3.png";
 import coco from "../Img/coco.png";
 import lol from "../Img/lol.png";
 import "../styles.css";
+function Principal() { 
 
-function Principal() {
   return (
+    
     <div>
       <h1 class="main"> Veja os principais jogos e streamers da twitch</h1>
-      <img src={png3} width="300" />
-      <img src={lol} width="350" />
-      <img src={coco} width="150" />
-      <h4 class="main">
+      <img src={png3} width="450" />
+      <img src={lol} width="450" />
+      <img src={coco} width="250" />
+      <h4 class="mainh4">
         Clique Nos Botões em azul na barra verde para selecionar o que deseja
         assistir!
       </h4>
     </div>
-  );
+    );
 }
 export default Principal;
